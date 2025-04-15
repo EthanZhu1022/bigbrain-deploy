d4ca8cd47a7a5370261d3aff222a5ee07427fd74
YichenZhu <z5610836@ad.unsw.edu.au>
2025-04-16 02:26:52 +1000

    Dashboard page rewrite

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,7 +1,11 @@
+import { Container } from 'react-bootstrap';
+
 function Dashboard(){
-    return (
-        <>Dashboard</>
-    )
+  return (
+    <Container className="mt-5 text-center">
+      <h1>Welcome to the Dashboard</h1>
+    </Container>
+  )
 }
 
 export default Dashboard;
